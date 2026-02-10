be9a81a58c251f54da68fc571c7de9af1c743b2f
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2026-02-10 18:34:08 +0100

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Comfortaa",
+            cssVariable: "--font-comfortaa"
+        }]
+	}
 });
