f7880f2a36a9d12ee06a35bab874b9b5024afff0
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2026-02-10 21:05:17 +0100

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://JohannesWeinbrecht.github.io',
-    base: 'JohannesWeinbrecht.github.io',
 	integrations: [mdx(), sitemap()],
 	experimental: {
         fonts: [{
